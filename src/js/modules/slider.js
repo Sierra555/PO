7428ee6a45147eaa5144223b86d22772e4e34bc8
Dara Stotska <dara6234@gmail.com>
2023-01-09 18:09:57 +0200

    fixed#3

--- src/js/modules/slider.js
+++ src/js/modules/slider.js
@@ -1,9 +1,8 @@
-
 const slider = tns({
-    container: '.js-examples__slider',
+    container: '.js-examples-slider',
     items: 3,
     slideBy: 1,
-    controlsContainer: '.js-examples__slider-controls',
+    controlsContainer: '.js-examples-slider-controls',
     mouseDrag: true,
     loop: true,
     responsive: {
@@ -20,22 +19,3 @@ const slider = tns({
     }
 });
 
-const buttons = document.querySelectorAll('.tns-nav button');
-buttons.forEach(btn => {
-    btn.classList.add('tns-nav__btn');
-
-});
-
-const arrowSvg = document.querySelectorAll('.js-control__svg'),
-    controls = document.querySelectorAll('.js-control__btn'),
-    path = document.querySelectorAll('.js-control__svg path');
-
-
-document.addEventListener('click', (e) => {
-    controls.forEach(item => item.classList.remove("is--active"));
-    arrowSvg.forEach(item => item.classList.remove("is--active"));
-    path.forEach(item => item.classList.remove("is--active"));
-    if (e.target.closest('.js-control__btn')) {
-        e.target.classList.add("is--active");
-    }
-});
